fa2cabd0fe95b6d5ad9a19c768ad4993a1659b6c
mohan3eddy <jaganreddy2498@gmail.com>
2022-06-15 12:51:34 +0530

    alll

---
+++ src/main/java/jigs/HelloWorld.java
@@ -0,0 +1,10 @@
+package jigs;
+
+import java.util.logging.Logger;
+
+public class HelloWorld {
+
+    public static void main(String... args) {
+        Logger logger=Logger.getLogger(HelloWorld.class.getName());
+        logger.info("This is a Hello World!");    }
+}
